2396a9a9a05058d9495d64cfcdf397de91a98233
gefgu <gefgu@hotmail.com>
2020-04-12 10:58:23 -0300

    basic working version

---
+++ lib/database_helpers.dart
@@ -0,0 +1,123 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+final String tableNotes = 'notes';
+final String columnId = "_id";
+final String columnTitle = "title";
+final String columnBody = "body";
+final String columnForeign = "foreignCategory";
+final String tableCategories = "categories";
+final String columnCategoryName = "category";
+
+class Note {
+  int id;
+  String title;
+  String body;
+  int categoryId;
+
+  Map<String, dynamic> toMap() {
+    var map = <String, dynamic>{
+      columnTitle: title,
+      columnBody: body,
+      columnForeign: categoryId,
+    };
+    if (id != null) {
+      map[columnId] = id;
+    }
+    return map;
+  }
+
+  Note();
+
+  Note.fromMap(Map<String, dynamic> map) {
+    id = map[columnId];
+    title = map[columnTitle];
+    body = map[columnBody];
+    categoryId = map[columnForeign];
+  }
+}
+
+class Category {
+  int id;
+  String name;
+
+  Map<String, dynamic> toMap() {
+    var map = <String, dynamic>{
+      columnCategoryName: name,
+    };
+    if (id != null) {
+      map[columnId] = id;
+    }
+    return map;
+  }
+
+  Category();
+
+  Category.fromMap(Map<String, dynamic> map) {
+    id = map[columnId];
+    name = map[columnCategoryName];
+  }
+}
+
+class DatabaseHelper {
+  static final _databaseName = "knight_note.db";
+  static final _databaseVersion = 1;
+
+  DatabaseHelper._privateConstructor();
+  static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
+
+  static Database _database;
+  Future<Database> get database async {
+    if (_database != null) return _database;
+    _database = await _initDatabase();
+    return _database;
+  }
+
+  _initDatabase() async {
+    String path = join(await getDatabasesPath(), _databaseName);
+    return await openDatabase(path,
+        version: _databaseVersion, onCreate: _onCreate);
+  }
+
+  Future _onCreate(Database db, int version) async {
+    await db.execute('''
+     CREATE TABLE $tableNotes (
+     $columnId INTEGER PRIMARY KEY AUTOINCREMENT,
+     $columnTitle TEXT NOT NULL,
+     $columnBody TEXT,
+     $columnForeign INTEGER REFERENCES $tableCategories($columnId)
+     )
+    ''');
+    await db.execute('''
+    CREATE TABLE $tableCategories (
+    $columnId INTEGER PRIMARY KEY AUTOINCREMENT,
+    $columnCategoryName TEXT NOT NULL
+    )
+    ''');
+  }
+
+  Future<List<Category>> getAllCategories() async {
+    Database db = await database;
+    List<Map<String, dynamic>> maps = await db.query(tableCategories);
+
+    return List.generate(maps.length, (i) {
+      return Category.fromMap(maps[i]);
+    });
+  }
+
+  Future<List<Note>> getAllNotes() async {
+    Database db = await database;
+    List<Map<String, dynamic>> maps = await db.query(tableNotes);
+
+    return List.generate(maps.length, (i) {
+      return Note.fromMap(maps[i]);
+    });
+  }
+
+  Future<int> insertNote(Note note) async {
+    Database db = await database;
+    int id = await db.insert(tableNotes, note.toMap());
+
+    return id;
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,176 @@
+import 'package:flutter/material.dart';
+import 'package:knightnotes/database_helpers.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Knight Notes',
+      home: NotesWidget(),
+    );
+  }
+}
+
+class NotesWidget extends StatefulWidget {
+  const NotesWidget({Key key}) : super(key: key);
+
+  @override
+  NotesWidgetState createState() => NotesWidgetState();
+}
+
+class NotesWidgetState extends State<NotesWidget> {
+  List<Note> _notes;
+  List<Category> _categories;
+
+  Future<dynamic> getStuffFromDatabase() async {
+    DatabaseHelper helper = DatabaseHelper.instance;
+    _categories = await helper.getAllCategories();
+    _notes = await helper.getAllNotes();
+    print([_categories, _notes].toString());
+    return [_categories, _notes];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        title: new Text("Knight Notes"),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: () {
+          _pushAddNoteScreen();
+        },
+        child: Icon(Icons.add),
+      ),
+      body: new FutureBuilder(
+        future: getStuffFromDatabase(),
+        builder: (context, snapshot) {
+          Widget widgetReturn;
+          if (snapshot.hasData) {
+            widgetReturn = ListView.builder(
+              padding: const EdgeInsets.all(16.0),
+              itemCount: _notes.length,
+              itemBuilder: (BuildContext context, int index) {
+                if (index < _notes.length) {
+                  return noteTile(_notes[_notes.length - 1 - index]);
+                }
+                return null;
+              },
+            );
+          } else if (snapshot.hasError) {
+            widgetReturn = Text(snapshot.error.toString());
+          } else {
+            widgetReturn = CircularProgressIndicator();
+          }
+          return widgetReturn;
+        },
+      ),
+    );
+  }
+
+  Widget noteTile(Note note) {
+    return FlatButton(
+      child: Container(
+        child: Column(
+          children: <Widget>[
+            new Text(
+              note.title,
+              style: Theme.of(context).textTheme.title,
+            ),
+            new SizedBox(
+              height: 6.0,
+            ),
+            new Text(note.body)
+          ],
+        ),
+        padding: const EdgeInsets.all(12.0),
+      ),
+      onPressed: () {
+        _pushNoteScreen(note);
+      },
+    );
+  }
+
+  void _pushNoteScreen(Note note) {
+    Navigator.of(context).push(new MaterialPageRoute(builder: (context) {
+      return new Scaffold(
+        appBar: new AppBar(
+          title: new Text(
+            "Note Screen",
+          ),
+        ),
+        body: Container(
+          padding: const EdgeInsets.all(16.0),
+          child: ListView(
+            children: <Widget>[
+              Text(
+                note.title,
+                style: Theme.of(context).textTheme.display1,
+              ),
+              SizedBox(
+                height: 32.0,
+              ),
+              Text(note.body)
+            ],
+          ),
+        ),
+      );
+    }));
+  }
+
+  void _pushAddNoteScreen() {
+    Note note = Note();
+    final _titleController = TextEditingController();
+    final _contentController = TextEditingController();
+
+    Navigator.of(context).push(new MaterialPageRoute(builder: (context) {
+      return new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Add Note"),
+        ),
+        body: Container(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: <Widget>[
+              TextField(
+                decoration: new InputDecoration(
+                  labelText: "Title",
+                ),
+                controller: _titleController,
+              ),
+              TextField(
+                maxLines: null,
+                keyboardType: TextInputType.multiline,
+                decoration: new InputDecoration(
+                  labelText: "Content",
+                ),
+                controller: _contentController,
+              ),
+              RaisedButton(
+                onPressed: () async {
+                  note.title = _titleController.text;
+                  note.body = _contentController.text;
+                  _addNote(note);
+                  Navigator.pop(context);
+                  print("Added note: " + note.toMap().toString());
+                },
+                child: Text("Submit"),
+              )
+            ],
+          ),
+        ),
+      );
+    }));
+  }
+
+  void _addNote(Note note) async {
+    DatabaseHelper helper = DatabaseHelper.instance;
+    setState(() async {
+      note.id = await helper.insertNote(note);
+    });
+  }
+}
